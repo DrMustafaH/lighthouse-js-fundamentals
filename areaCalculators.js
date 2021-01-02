bd69dbafd0cf0fde87468473608566b92c5d54c1
DrMustafaH <mustafaharith@gmail.com>
2021-01-01 18:14:44 -0800

    try no pass ssh

--- areaCalculators.js
+++ areaCalculators.js
@@ -20,7 +20,7 @@ const calculateCircleArea = function (radius) {
 
 
 
-console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(10, 5));     // should print  50
 console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
 console.log(calculateRectangleArea(10, -5));    // should print undefined
 
